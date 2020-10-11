9756659d4ad6fff1edfa2c6d2904c49a275e049d
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2020-10-11 19:22:58 -0300

    Contacts

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -2,12 +2,41 @@ import React from 'react';
 
 import { Wrapper, LeftContainer, RightContainer } from './styles';
 
+import Phonelogo from '../../assets/logos/Phone_logo.png';
+import WhatsLogo from '../../assets/logos/Whatsapp_logo.png';
+import MailLogo from '../../assets/logos/Email_logo.png';
+
 const Contact = () => (
   <Wrapper>
     <LeftContainer>
       <strong>ENTRE EM CONTATO</strong>
+      <div>
+        <span>
+          <img src={Phonelogo} alt="Telefone" width="60px" />
+          <p>+55071999080236</p>
+        </span>
+        <span>
+          <img src={WhatsLogo} alt="Telefone" width="60px" />
+          <a target="_blank" rel="noopener noreferrer" href="https://api.whatsapp.com/send?phone=5571999080236&text=Ola!%20Gostaria%20de%20falar%20com%20voc%C3%AA!">Fale comigo pelo Whatsapp!</a>
+        </span>
+        <span>
+          <img src={MailLogo} alt="Telefone" width="60px" />
+          <p>geisweiller@gmail.com</p>
+        </span>
+      </div>
     </LeftContainer>
-    <RightContainer />
+    <RightContainer>
+      <div>
+        <span>
+          <input type="text" placeholder="Nome" />
+          <input type="text" placeholder="Sobrenome" />
+        </span>
+        <span className="message">
+          <textarea placeholder="Sua mensagem" />
+          <button type="button">Envie sua mensagem!</button>
+        </span>
+      </div>
+    </RightContainer>
   </Wrapper>
 );
 
--- src/pages/Contact/styles.js
+++ src/pages/Contact/styles.js
@@ -23,6 +23,67 @@ export const RightContainer = styled.div`
   height: auto;
   top:0;
   padding: 40px;
+
+  div {
+
+    display: flex;  
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 80px;
+  }
+
+  span {
+    margin: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center ;
+  }
+
+  input {
+    padding: 10px;
+    margin: 10px;
+  }
+
+  input::placeholder {
+      color:black;
+      opacity: 0.3;
+    }
+
+  span.message {
+    flex-direction: column;
+
+    button {
+      background: #393636;
+      color: #FFF;
+      font-weight: bold;
+      padding: 20px;
+      border-radius: 10px;
+      border: 0;
+      position:absolute;
+      bottom: 100px;
+      box-shadow: 0 4px 8px 0 rgba(0,0,0,0.5);
+      margin-top: 20px;
+    }
+    
+    button:hover{
+      box-shadow: 0 8px 16px 0 rgba(0,0,0,0.7)
+    }
+
+    textarea {
+      resize:none;
+      width: 380px;
+      height: 200px;
+      font-family: "Roboto", sans-serif;
+    }
+
+    textarea::placeholder {
+      color:black;
+      opacity: 0.3;
+      padding: 10px;
+      font-size:14px;
+    }
+  }
 `;
 
 export const LeftContainer = styled.div`
@@ -30,10 +91,44 @@ export const LeftContainer = styled.div`
   display: flex;
   width:50%;
   position: absolute;
-  align-items: center;
   height: 100%;
-  justify-content: center;
   left:0;
   padding: 40px;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  strong {
+    font-size: 40px;
+    margin: 60px;
+  }
+
+  div {
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin: 80px;
+  }
+
+  span {
+    margin: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center ;
+  }
+
+  p, a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 10px;
+  }
+
+  a:hover, a:active {
+    font-weight: 700;
+    text-decoration-color: black;
+    transition: 0.3s;
+    border-radius: 10px;
+  }
 `;
