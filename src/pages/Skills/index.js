899bf7ffbfe5f1c3e69caff704ce1cf8cf9ce6c1
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2020-10-13 18:26:57 -0300

    Conteúdo

--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -1,46 +1,72 @@
+/* eslint-disable max-len */
 import React from 'react';
 
 import { Wrapper, TechContainer, Tech } from './styles';
-import logo from '../../assets/logos/React_logo.png';
+import logoReact from '../../assets/logos/React_logo.png';
+import logoJs from '../../assets/logos/Js_logo.png';
+import logoNode from '../../assets/logos/Node_logo.png';
+import logoUX from '../../assets/logos/UX_logo.png';
 
 const Skills = () => (
   <Wrapper>
     <TechContainer>
       <Tech>
-        <img src={logo} alt="React" width="100px" />
-        <strong>
-          ReactJs
-        </strong>
-        <p>
-          Utilizo esse framework para construções de aplicações Web.
-        </p>
+        <div>
+          <img src={logoJs} alt="React" width="100px" />
+          <strong>
+            JavaScript
+          </strong>
+        </div>
+        <div>
+          <p>
+            Possuo conhecimento avançado nessa linguagem, bem como os conceitos trazidos pelo
+            ES6+, como: Webpack, Classes, Arrow functions, Desestruturação, Import/Export, Async/Await, Rest/Spread.
+          </p>
+        </div>
       </Tech>
       <Tech>
-        <img src={logo} alt="React" width="100px" />
-        <strong>
-          ReactJs
-        </strong>
-        <p>
-          Utilizo esse framework para construções de aplicações Web.
-        </p>
+        <div>
+          <img src={logoReact} alt="React" width="100px" />
+          <strong>
+            ReactJs e React Native
+          </strong>
+        </div>
+        <div>
+          <p>
+            Meu maior foco de aprendizado. Desenvolvi esse portfólio atráves do ReactJs justamente para apresentar
+            meu conhecimento da ferramenta, utilizei conceitos de SPA e Stateful Components. A cada dia procuro aprender mais sobre essa biblioteca que torna mais simples
+            o desenvolvimento de interfaces Web e Mobile.
+          </p>
+        </div>
       </Tech>
       <Tech>
-        <img src={logo} alt="React" width="100px" />
-        <strong>
-          ReactJs
-        </strong>
-        <p>
-          Utilizo esse framework para construções de aplicações Web.
-        </p>
+        <div>
+          <img src={logoNode} alt="React" width="100px" />
+          <strong>
+            NodeJs
+          </strong>
+        </div>
+        <div>
+          <p>
+            Apesar de não ser o foco da Stack de Frontend, possuo conhecimento básico nessa tecnologia.
+            Já desenvolvi algumas aplicações utilizando o NodeJs.
+          </p>
+        </div>
       </Tech>
       <Tech>
-        <img src={logo} alt="React" width="100px" />
-        <strong>
-          ReactJs
-        </strong>
-        <p>
-          Utilizo esse framework para construções de aplicações Web.
-        </p>
+        <div>
+          <img src={logoUX} alt="React" width="100px" />
+          <strong>
+            UI/UX
+          </strong>
+        </div>
+        <div>
+          <p>
+            Por ser um desenvolvedor Frontend, me interesso bastante pelos conceitos UI/UX.
+            Já participei de cursos referentes ao assunto, para entender melhor o processo e
+            entregar resultados mais satisfatórios.
+          </p>
+        </div>
       </Tech>
     </TechContainer>
   </Wrapper>
