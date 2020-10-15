0b03173c6b2e48aac8c84c232400d64a705d3dc5
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2020-10-14 21:26:32 -0300

    New Icon/Logo

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,9 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Audiowide&display=swap');
 
+ 
 
 * {
   margin: 0;
