39d30cb74367fc6e65306a62b53beaf94966bf26
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2020-10-26 23:52:39 -0300

    Switch Button

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Switch from '@material-ui/core/Switch';
+
 import { FooterWrapper } from './styles';
 
-const Footer = () => (
-  <FooterWrapper>
-    <p>copyright © Arthur Geisweiller</p>
-    <nav>
-      <a href="https://drive.google.com/uc?export=download&id=1et02y71sWvNlofGHYrUT0Y_7EPVfBluB">Baixe meu CV</a>
-      <a href="https://www.linkedin.com/in/arthur-geisweiller-414587b7/" alt="Linkedin">Linkedin</a>
-      <a href="https://www.facebook.com/arthurgeisweiller/" alt="Facebook">Facebook</a>
-      <a href="https://www.instagram.com/arthurgeisweiller" alt="Instagram">Instagram</a>
-    </nav>
-  </FooterWrapper>
+export default function Footer() {
+  const [darkMode, setDarkMode] = useState(false);
 
-);
+  function darkModeFunction() {
+    if (darkMode === false) {
+      setDarkMode(true);
+    } else {
+      setDarkMode(false);
+    }
+  }
 
-export default Footer;
+  return (
+    <FooterWrapper>
+      <Switch
+        color="default"
+        onChange={darkModeFunction}
+      />
+      <nav>
+        <a href="https://drive.google.com/uc?export=download&id=1et02y71sWvNlofGHYrUT0Y_7EPVfBluB">Baixe meu CV</a>
+        <a href="https://www.linkedin.com/in/arthur-geisweiller-414587b7/" alt="Linkedin">Linkedin</a>
+        <a href="https://www.facebook.com/arthurgeisweiller/" alt="Facebook">Facebook</a>
+        <a href="https://www.instagram.com/arthurgeisweiller" alt="Instagram">Instagram</a>
+      </nav>
+    </FooterWrapper>
+  );
+}
